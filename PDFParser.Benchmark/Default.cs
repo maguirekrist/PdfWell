4242a08f4e80b32bbe64d59078fdd6538c67a7c7
Maguire Krist <maguirekrist@gmail.com>
2025-07-17 09:26:39 -0400

    fixed benchmark being included in release, added unit test badge

--- PDFParser.Benchmark/Default.cs
+++ PDFParser.Benchmark/Default.cs
@@ -9,17 +9,14 @@ namespace PDFParser.Benchmark;
 public class DefaultBenchmark
 {
     private const string A4 = "TestPDFs/A4.pdf";
-    private const string Statement = "TestPDFs/Statement.pdf";
 
     private byte[] _a4Bytes = [];
-    private byte[] _statementBytes = [];
     
     
     [GlobalSetup]
     public void SetDefaultBenchmark()
     {
         _a4Bytes = File.ReadAllBytes(A4);
-        _statementBytes = File.ReadAllBytes(Statement);
     }
 
     [Benchmark]
@@ -31,22 +28,11 @@ public class DefaultBenchmark
         // var pages = doc.Pages;
         // var textPageOne = pages[0].Texts;
     }
-
-    [Benchmark]
-    public object ParseStatement()
-    {
-        var parser = new PdfParser(_statementBytes);
-        var doc = parser.Parse();
-        return doc;
-        // //
-        // var pages = doc.Pages;
-        // var textPageOne = pages[0].Texts;
-    }
     
     [Benchmark]
     public object ParseStatementKmp()
     {
-        var parser = new PdfParser(_statementBytes, new KMPByteMatcher());
+        var parser = new PdfParser(_a4Bytes, new KMPByteMatcher());
         var doc = parser.Parse();
         return doc;
         // //
@@ -57,7 +43,7 @@ public class DefaultBenchmark
     [Benchmark]
     public object ParseStatementSunday()
     {
-        var parser = new PdfParser(_statementBytes, new BoyerMooreMatcher());
+        var parser = new PdfParser(_a4Bytes, new BoyerMooreMatcher());
         var doc = parser.Parse();
         return doc;
         // //
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Currently a work in-progress, but this library is an attempt to make a simple and fast PDF parser that can handle mutable AcroForms; as well as other features.
 
 ![GitHub release (latest by date)](https://img.shields.io/github/v/release/maguirekrist/PdfWell)
+[![Run Unit Tests](https://github.com/maguirekrist/PdfWell/actions/workflows/run-tests.yml/badge.svg)](https://github.com/maguirekrist/PdfWell/actions/workflows/run-tests.yml)
